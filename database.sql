2958a80920408e9ab4116075f7ed1c7f3495d5d4
clementgarrigues <clement@PomPomPom.lan>
2019-02-05 14:28:22 +0100

    Added index.php and database.sql

--- database.sql
+++ database.sql
@@ -1,2 +1,3 @@
-CREATE DATABASE db_devopstp
-CREATE TABLE tbl_devopstp
+CREATE DATABASE devops_tp1;
+CREATE TABLE `devops_tp1`.`compteur` ( `id` INT NOT NULL AUTO_INCREMENT , `value` INT NOT NULL , PRIMARY KEY (`id`));
+INSERT INTO `devops_tp1`.`compteur` (`id`,`value`) VALUES (1,0);
